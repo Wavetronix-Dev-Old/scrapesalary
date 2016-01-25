b1ff7c7d6a0b19be6ad9539a0e4359fd3bd1b0dd
Elizabeth Huntsman <ehuntsman@Elizabeths-MacBook-Pro.local>
2016-01-25 16:10:54 -0700

    salary updated

---
+++ nokogiri_test.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'watir-webdriver'
+require 'phantomjs'
+
+url = "http://swz.salary.com/SalaryWizard/Engineer-III-Salary-Details-Provo-UT.aspx"
+
+browser = Watir::Browser.new(:phantomjs)
+
+browser.goto(url)
+
+puts browser.div(:id => 'salarydetail').text
+puts browser.td(:id => 'marketv10').text
+puts browser.td(:id => 'marketv25').text
+puts browser.div(:id => 'mediansalary').text
+puts browser.td(:id => 'marketv75').text
+puts browser.td(:id => 'marketv90').text
